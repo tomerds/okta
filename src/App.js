bdc3cd3c4c040607c6742f00a8a932a349ff72ed
tomerds <tomerdusautoy@gmail.com>
2020-01-31 15:18:42 +0000

    client login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, ImplicitCallback } from '@okta/okta-react';
+import Home from './components/login-button';
+
+const config = {
+  issuer: 'https://dev-811688/oauth2/default',
+  redirectUri: window.location.origin + '/implicit/callback',
+  clientId: '0oa16o5teu3VycONs4x6',
+  pkce: true
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Security {...config}>
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/implicit/callback' component={ImplicitCallback}/>
+        </Security>
+      </Router>
+    );
+  }
 }
 
 export default App;
